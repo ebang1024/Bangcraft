e25d501e9d3cb131771135035f65a1748f954540
Nikki Olsen <nolsen1275@jh-mba158-20.d214.org>
2018-05-07 14:59:17 -0500

    bang bear added

--- Bangcraft/GameScene.swift
+++ Bangcraft/GameScene.swift
@@ -9,10 +9,30 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+struct PhysicsCategory {
+    static let None : UInt32 = 0
+    static let All : UInt32 = UInt32.max
+    static let Monster : UInt32 = 0b1      // is equal to binary 1
+    static let Projectile: UInt32 = 0b10      // is equal to binary 2
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    // creating player sprite
+
+    let player = SKSpriteNode(imageNamed: "bangbear")
+ 
+    override func didMove(to view: SKView)
+        
+    {
+        //placing our player sprite
+        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        
+        //adding the sprite
+        addChild(player)
+        
+     //   let backgroundMusic = SKAudioNode(fileNamed: "gameMusic.mp3")
+     //   backgroundMusic.autoplayLooped = true
+      //  addChild(backgroundMusic)
+        
+}
 }
