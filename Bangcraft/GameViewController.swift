484e1c1990d7793569b8aa37f7557bee1927d1c3
Nikki Olsen <nolsen1275@jh-mba158-20.d214.org>
2018-05-07 15:07:53 -0500

    levi shut up challenge

--- Bangcraft/GameViewController.swift
+++ Bangcraft/GameViewController.swift
@@ -14,7 +14,13 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene(size:view.bounds.size)
+        let skView = view as! SKView
         
-
+        //view fps
+        skView.showsFPS = true
+        //the scale of the game scene
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
 }
 }
